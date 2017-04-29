cf96db1c77e7fcae2e963e26e1e9dee4a2e69e4a
Dennis Eisen <dennis.eisen@t-online.de>
2017-04-29 20:25:23 +0200

    Implemented support for ".tar.gz" archives and added push notification support.
    + implemented ".tar.gz" support
    + added push notification
    + added locking (to prevent concurrent update)

--- update.php
+++ update.php
@@ -1,25 +1,30 @@
 <?php
+
 /**	ChurchTools - Auto Updater
  *	@copyright: Copyright (c) 2016, Dennis Eisen & Michael Lux
- *	@version: 27.05.2016, 17:08
+ *	@version: 29.05.2016
  */
 
+header ('Content-Type: text/plain; charset=utf-8');
+
+// Pushover and Pushbullet integration
+if (file_exists('push.inc.php')) {
+	require 'push.inc.php' ;
+}
+
 // Put in your own password hash here
-define('HASH', 'PUT IN YOUR OWN HASH HERE');
+define('HASH', 			'...');
 // Modify to correct seafile server URL here
-define('SEAFILE_DIR', '/d/xyz1234567/');
-
+define('SEAFILE_DIR', 	'd/.../');
 // Should be fine, except if JMR decides to change the location of the SeaFile server... ;)
-define('SEAFILE_URL', 'https://seafile.churchtools.de' . SEAFILE_DIR);
+define('SEAFILE_URL', 	'https://seafile.churchtools.de/' . SEAFILE_DIR);
 
-header('Content-Type: text/plain; charset=utf-8');
+echo '### ChurchTools - Auto Updater ###', "\n\n";
 
 // Password protection via QUERY_STRING
 if (!password_verify($_SERVER['QUERY_STRING'], HASH)) {
 	exit('Try harder! ;)');
 }
-
-echo '### ChurchTools - Auto Updater ###', "\n\n";
 
 // Keyword for cronejob e-mail notification. No e-mail will be sent if detected!
 register_shutdown_function(function () {
@@ -28,25 +33,51 @@ register_shutdown_function(function () {
 	}
 });
 
+$lockFile = __DIR__ . '/ctupdate.lock';
+$ignoreLock = false;
+$acquiredLock = false;
 try {
+	// Check whether lock should be ignored because it's old (older than 10 minutes)
+	if (file_exists($lockFile) && filemtime($lockFile) < time() - 600) {
+		echo 'Lock is older than 10 minutes, continue ignoring lock.', "\n";
+		$ignoreLock = true;
+	}
+	$lock = fopen($lockFile, 'w');
+	// Proceed if lock can be acquired or lock should be ignored
+	$acquiredLock = flock($lock, LOCK_EX | LOCK_NB);
+	if(!$acquiredLock && !$ignoreLock) {
+		throw new Exception('Update already in progress!');
+	}
+
     $updateArchive = __DIR__ . '/update.zip';
 	// Download zip file from Seafile server
 	for ($tries = 0; $tries < 3 && !file_exists($updateArchive); $tries++) {
-        list($downloadURL, $ext) = getDownloadURL();
-        $updateArchive = __DIR__ . '/update' . $ext;
+		list($downloadURL, $ext) = getDownloadURL();
+		$updateArchive = __DIR__ . '/update' . $ext;
 		copy($downloadURL, $updateArchive);
 	}
 	// Extract files
 	updateSystem($updateArchive);
 } catch(Exception $e) {
-	echo $e->getMessage() . "\n";
-	return;
+	echo $e->getMessage(), "\n";
+} finally {
+	if ($acquiredLock || $ignoreLock) {
+		// Unlock script if lock was actually acquired or ignored
+		flock($lock, LOCK_UN);
+		fclose($lock);
+		unlink($lockFile);
+	} else {
+		// Always close the file handle
+		fclose($lock);
+	}
 }
 
 // Build download link
-function getDownloadURL($url = 'https://seafile.churchtools.de/d/2ff6acb81e/') {
-	$html = file_get_contents(SEAFILE_URL);
-	if (preg_match('#href="' . SEAFILE_DIR . '(files/\?p=/churchtools-(3\..+?)(\.zip|\.tar\.gz))".*?<time[^<]+title="([^"]+?)"#s', $html, $matches)) {
+function getDownloadURL($url = SEAFILE_URL) {
+	$html = file_get_contents($url);
+	if (preg_match('#href="/d/2ff6acb81e/(files/\?p=/churchtools-(3\..+?)(\.zip|\.tar\.gz))".*?<time[^<]+title="([^"]+?)"#s',
+			$html, $matches)) {
+		define('CT_VERSION', $matches[2]);
 		// Parse SeaFile timestamp
 		$ts = DateTime::createFromFormat(DateTime::RFC2822, $matches[4])->getTimeStamp();
 		// If SeaFile archive is older than modification date of constants.php, don't perform update
@@ -55,36 +86,53 @@ function getDownloadURL($url = 'https://seafile.churchtools.de/d/2ff6acb81e/') {
 		}
 		return array($url . $matches[1] . '&dl=1', $matches[3]);
 	} else {
+		if (function_exists('push')) {
+			push('[Fehler] Download', "Kein gültiger ChurchTools 3 Download im HTML gefunden! <font color=\"red\">$url</font>", 1);
+		}
 		throw new Exception('No valid ChurchTools 3 download found in HTML!');
 	}
 }
 
 // Recursive deleting of directorys
-function delTree($dir) { 
-	$files = array_diff(scandir($dir), ['.','..']); 
-	foreach ($files as $file) { 
-		is_dir("$dir/$file") ? delTree("$dir/$file") : unlink("$dir/$file"); 
+function delTree($dir) {
+	$files = array_diff(scandir($dir), ['.','..']);
+	foreach ($files as $file) {
+		is_dir("$dir/$file") ? delTree("$dir/$file") : unlink("$dir/$file");
 	}
 	return rmdir($dir);
-} 
+}
 
 // Extract 'system' and 'index.php' or trigger error
 function updateSystem($updateArchive) {
 	$zip = new PharData($updateArchive);
     $zip->extractTo(__DIR__);
-    
-    if (!(file_exists(__DIR__ . '/churchtools') && is_dir(__DIR__ . '/churchtools'))) {
+	$needle = 'churchtools';
+	$dirName = null;
+	foreach (scandir('phar:///' . $updateArchive) as $entry) {
+		if (substr($entry, 0, strlen($needle)) === $needle) {
+			$dirName = $entry;
+		}
+	}
+
+    if (!(file_exists(__DIR__ . '/' . $dirName) && is_dir(__DIR__ . '/' . $dirName))) {
         trigger_error('The ZIP archive does not contain directory "churchtools", or creation failed!', E_USER_ERROR);
+		if (function_exists('push')) {
+			push('[Fehler] ZIP', 'Das Verzeichnis "churchtools" fehlt im ZIP Archiv!', 1);
+		}
         throw new Exception('The ZIP archive does not contain directory "churchtools", or creation failed!');
     }
-    
+
     // Check if directory system exists, if yes, delete it
     if (file_exists(__DIR__ . '/system')) delTree(__DIR__ . '/system');
-    
-    rename(__DIR__ . '/churchtools/system', __DIR__ . '/system');
-    rename(__DIR__ . '/churchtools/index.php', __DIR__ . '/index.php');
-    delTree(__DIR__ . '/churchtools');
-    
+
+    rename(__DIR__ . '/' . $dirName . '/system', __DIR__ . '/system');
+    rename(__DIR__ . '/' . $dirName . '/index.php', __DIR__ . '/index.php');
+    delTree(__DIR__ . '/' . $dirName);
+
+	if (function_exists('push')) {
+		push('Update erfolgreich', 'Ein Update wurde erfolgreich installiert: <b>' . CT_VERSION . '</b>!');
+	}
+
 	if (file_exists($updateArchive)) {
 		unlink($updateArchive);
 	}
